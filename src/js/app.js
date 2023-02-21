fb1625200fbfe32207978dd198406378206503a9
roman <usankin.roma@gmail.com>
2023-02-21 16:00:18 +0400

    finished popular

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    navigation: {
+        nextEl: '.popular-slider__arrow--next',
+        prevEl: '.popular-slider__arrow--prev',
+    }
+});
