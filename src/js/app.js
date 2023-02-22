de26210b85aa772002862bd84057625f45aeabba
roman <usankin.roma@gmail.com>
2023-02-22 16:22:26 +0400

    work in slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,24 +2,35 @@ document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper', {
         slidesPerView: 3,
         loop: true,
-        freeMode: true,
         navigation: {
             nextEl: '.popular-slider__arrow--next',
             prevEl: '.popular-slider__arrow--prev',
+        },
+        breakpoints: {
+            300: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            575: {
+                slidesPerView: "auto"
+            },
+            1200: {
+                slidesPerView: 3,
+            },
         }
     });
 
     let burger = document.querySelector('.hamburger');
     let mobile = document.querySelector('.mobile');
     burger.addEventListener('click', () => {
-        if (burger.classList.contains('active')){
+        if (burger.classList.contains('active')) {
             burger.classList.remove('active');
             mobile.classList.remove('active');
-            document.body.style.overflow="visible";
+            document.body.style.overflow = "visible";
         } else {
             burger.classList.add('active');
             mobile.classList.add('active');
-            document.body.style.overflow="hidden";
+            document.body.style.overflow = "hidden";
         }
 
     });
