32bb1d6f1920491bc886667460896535c1b4efbe
roman <usankin.roma@gmail.com>
2023-02-22 13:11:06 +0400

    finished mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,25 @@
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    loop: true,
-    navigation: {
-        nextEl: '.popular-slider__arrow--next',
-        prevEl: '.popular-slider__arrow--prev',
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 3,
+        loop: true,
+        navigation: {
+            nextEl: '.popular-slider__arrow--next',
+            prevEl: '.popular-slider__arrow--prev',
+        }
+    });
+
+    let burger = document.querySelector('.hamburger');
+    let mobile = document.querySelector('.mobile');
+    burger.addEventListener('click', () => {
+        if (burger.classList.contains('active')){
+            burger.classList.remove('active');
+            mobile.classList.remove('active');
+            document.body.style.overflow="visible";
+        } else {
+            burger.classList.add('active');
+            mobile.classList.add('active');
+            document.body.style.overflow="hidden";
+        }
+
+    });
 });
